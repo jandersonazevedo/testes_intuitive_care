39e22c7a6c3afbbeb2fa0d27d24e09834a4eb1e0
Janderson Azevedo <janderson.na.ja@gmail.com>
2022-06-02 12:19:54 -0300

    feat: função de pesquisar e deletar funcionando

--- client/src/components/Table.vue
+++ client/src/components/Table.vue
@@ -1,34 +1,42 @@
 <template>
   <div class="container-table">
-    <h1 class="table-title">{{ title }}</h1>
+    <h1 class="table-title">Relação de Operadoras Ativas ANS</h1>
 
     <div class="search-bar">
-      <input type="text" placeholder="Digite sua pesquisa aqui" />
-      <input type="submit" value="Pesquisar" />
+      <form @submit="getPesquisa">
+        <input
+          type="text"
+          name="pesquisa"
+          placeholder="Digite sua pesquisa aqui"
+        />
+        <input type="submit" value="Pesquisar" />
+      </form>
     </div>
 
     <table>
       <thead>
-        <th>Registro ANS</th>
+        <th @click="orderBy">Registro ANS</th>
         <th>CNPJ</th>
         <th>Nome Fantasia</th>
-        <th>Modalidade</th>
         <th>Telefone</th>
         <th>E-mail</th>
+        <th></th>
       </thead>
       <tbody>
         <tr v-for="op in operadoras" :key="op.id">
           <td>{{ op.registro_ans }}</td>
           <td>{{ op.cnpj }}</td>
           <td>{{ op.nome_fantasia }}</td>
-          <td>{{ op.modalidade }}</td>
           <td>{{ op.telefone }}</td>
           <td>{{ op.endereco_eletronico }}</td>
+          <td>
+            <button @click="deletar(op.id)">Deletar</button>
+          </td>
         </tr>
       </tbody>
     </table>
 
-    <button @click="carregarArquivo" v-show="operadoras">
+    <button @click="restaurarDados" v-show="operadoras">
       Restaurar dados iniciais
     </button>
   </div>
@@ -41,19 +49,41 @@ export default {
   name: "Table",
   data() {
     return {
-      title: null,
       operadoras: null,
     };
   },
   methods: {
-    getOperadoras() {
-      api
+    async getOperadoras() {
+      await api
         .get("/operadoras")
         .then((response) => (this.operadoras = response.data));
     },
-    carregarArquivo() {
-      api.post("/carregar-arquivo");
+
+    async restaurarDados() {
+      await api.delete("/limpar-dados");
+      await api.post("/carregar-arquivo");
       this.getOperadoras();
+    },
+
+    async getPesquisa(event) {
+      event.preventDefault();
+
+      const pesquisa = event.target.pesquisa.value;
+
+      api.get(`/pesquisar-operadora/${pesquisa}`).then(({ data }) => {
+        this.operadoras = data;
+      });
+    },
+
+    async deletar(id) {
+      if (confirm(`Tem certeza que deseja delatar esta operadora?`)) {
+        api.delete(`/deletar-operadora/${id}`);
+        this.getOperadoras();
+      }
+    },
+
+    orderBy() {
+      console.log(this.operadoras);
     },
   },
   mounted() {
@@ -79,11 +109,12 @@ export default {
 
 .search-bar {
   grid-area: div;
+  display: flex;
+  justify-content: flex-end;
 }
 
 table {
   grid-area: table;
-  height: 100px;
   text-align: left;
   border-collapse: collapse;
 }
@@ -96,5 +127,26 @@ th {
   padding: 5px;
   font-size: 15px;
   color: #333;
+  cursor: pointer;
+  transition: 0.2s;
+}
+
+th:hover {
+  color: #000;
+  background-color: #eee;
+}
+
+tr {
+  transition: 0.1s;
+  cursor: pointer;
+}
+
+tr:hover {
+  background-color: #eee;
+}
+
+td {
+  padding: 10px 5px;
+  font-size: 14px;
 }
 </style>
--- server/index.js
+++ server/index.js
@@ -46,4 +46,46 @@ app.post("/carregar-arquivo", (req, res) => {
   });
 });
 
+app.delete("/limpar-dados", (req, res) => {
+  SQL = "TRUNCATE operadoras_ativas_ans";
+  db.query(SQL, (err, results) => {
+    if (err) res.send(err);
+    res.send(JSON.stringify(results));
+  });
+});
+
+app.get("/pesquisar-operadora/:pesquisa", (req, res) => {
+  const p = `%${req.params.pesquisa.toLowerCase()}%`;
+  const SQL = `SELECT * FROM operadoras_ativas_ans WHERE registro_ans 
+  LIKE '${p}' OR cnpj LIKE '${p}' OR razao_social LIKE '${p}' OR nome_fantasia 
+  LIKE '${p}' OR modalidade LIKE '${p}' OR logradouro LIKE '${p}' OR numero 
+  LIKE '${p}' OR complemento LIKE '${p}' OR bairro LIKE '${p}' OR cidade 
+  LIKE '${p}' OR uf LIKE '${p}' OR cep LIKE '${p}' OR ddd LIKE '${p}' OR telefone 
+  LIKE '${p}' OR fax LIKE '${p}' OR endereco_eletronico LIKE '${p}' OR representante 
+  LIKE '${p}' OR cargo_representante LIKE '${p}' OR data_registro_ans LIKE '${p}'`;
+
+  db.query(SQL, (err, result) => {
+    if (err) res.send(err);
+    res.send(JSON.stringify(result));
+  });
+});
+
+app.get("/pesquisar-operadora", (req, res) => {
+  const SQL = `SELECT * FROM operadoras_ativas_ans`;
+
+  db.query(SQL, (err, result) => {
+    if (err) res.send(err);
+    res.send(JSON.stringify(result));
+  });
+});
+
+app.delete("/deletar-operadora/:id", (req, res) => {
+  SQL = "DELETE FROM operadoras_ativas_ans WHERE id = ?";
+
+  db.query(SQL, req.params.id, (err, result) => {
+    if (err) res.send(err);
+    res.send(JSON.stringify(result));
+  });
+});
+
 app.listen(3000, () => console.log("Servidor em execução."));
